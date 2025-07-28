0ce9e9ac41926e8e60a5f911ae90123473227b00
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2025-07-28 16:10:18 -0400

    Update DataManipulation.py

--- lib/DataManipulation.py
+++ lib/DataManipulation.py
@@ -9,5 +9,5 @@ def join_orders_customers(orders_df, customers_df):
 def count_orders_state(joined_df):
     return joined_df.groupBy('state').count()
 
-def filter_orders_generic(orders_df,status):
-    return orders_df.filter("order_status = '{}'".format(status))
+# def filter_orders_generic(orders_df,status):
+#     return orders_df.filter("order_status = '{}'".format(status))
