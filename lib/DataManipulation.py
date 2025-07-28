3527c2166e7637f0f2d04cba8e94de3a1c1b07f9
Dixit8112 <dixititaliya5992@gmail.com>
2025-07-28 17:09:57 -0400

    comment

--- lib/DataManipulation.py
+++ lib/DataManipulation.py
@@ -6,10 +6,10 @@ def filter_closed_orders(orders_df):
 def join_orders_customers(orders_df, customers_df):
     return orders_df.join(customers_df, "customer_id")
 
-# def count_orders_state(joined_df):
-#     return joined_df.groupBy('state').count()
+def count_orders_state(joined_df):
+    return joined_df.groupBy('state').count()
 
-# def filter_orders_generic(orders_df,status):
-#     return orders_df.filter("order_status = '{}'".format(status))
+def filter_orders_generic(orders_df,status):
+    return orders_df.filter("order_status = '{}'".format(status))
 
-# hello world
+hello world
